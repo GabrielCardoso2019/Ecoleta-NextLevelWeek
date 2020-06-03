44fa0a8281efd0d8d3cc30c7a90a5ae15a4c8c11
GabrielCardoso2019 <gabrielcardososs2016@hotmail.com>
2020-06-03 12:59:35 -0300

    primeira parte do Back-End da página de cadastro. CSS finalizado da página de cadastro

--- js/create-point.js
+++ js/create-point.js
@@ -1,5 +1,5 @@
 function populateUFs() {
-    const ufSelect = document.querySelector("select[name=uf]");
+    const ufSelect = document.querySelector("[name=uf]");
     fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
         .then(res => res.json())
         .then(states => {
@@ -21,14 +21,17 @@ function getCities(event) {
     const indexOfSelectedState = event.target.selectedIndex
     stateInput.value = event.target.options[indexOfSelectedState].text;
 
-    const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/33/municipios`;
+    const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`;
+
+    citySelect.innerHTML = "<option value>Selecione a Cidade</option>";
+    citySelect.disabled = true;
 
     fetch(url)
         .then(res => res.json())
         .then(cities => {
 
             for (const city of cities) {
-                citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`;
+                citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`;
             }
 
             citySelect.disabled = false;
@@ -37,5 +40,57 @@ function getCities(event) {
 
 
 document
-    .querySelector("select[name=uf]")
+    .querySelector("[name=uf]")
     .addEventListener("change", getCities);
+
+
+
+// Itens de Coleta
+// Pegar todos os Li's
+
+const itemsToCollect = document.querySelectorAll(".items-grid li");
+
+for (const item of itemsToCollect) {
+    item.addEventListener("click", handleSelectedItem)
+}
+
+
+const collectedItems = document.querySelector("input[name=items]");
+let selectedItems = [];
+
+
+function handleSelectedItem(event) {
+    const itemLi = event.target;
+
+    // Adciona ou remover uma classe JS
+    itemLi.classList.toggle("selected");
+
+    const itemId = itemLi.dataset.id;
+
+    // Verifica se existem selecionador
+    // Sim ? Pegar itens selecionados
+    const alreadySelected = selectedItems.findIndex(item => {
+        const itemFound = item == itemId; // Retorna True or False
+        return itemFound;
+    });
+
+    // Se estiver selecionado
+    if (alreadySelected >= 0) {
+        //  retirar da seleção
+        const filteredItems = selectedItems.filter(item => {
+            const itemIsDifferent = item != itemId; // False
+            return itemIsDifferent;
+        });
+
+        selectedItems = filteredItems;
+    } else {
+        // Se não estiver selecionado
+        // add à seleção
+        selectedItems.push(itemId)
+
+    }
+
+    // Atualizar o campo escondigo com os dados selecionados
+    collectedItems.value = selectedItems;
+
+}

f50e4092a19367f115e40c7a384e6b6d5c0bee4a
GabrielCardoso2019 <gabrielcardososs2016@hotmail.com>
2020-06-06 14:57:28 -0300

    adicionado Template Engine nas paginas html

--- src/server.js
+++ src/server.js
@@ -1,5 +1,36 @@
-const express = require("express")
-const server = express()
+const express = require("express");
+const server = express();
+
+// COnfiguração da pasta Public
+server.use(express.static("public"));
+
+// Template Engine
+const nunjucks = require("nunjucks");
+nunjucks.configure("src/view", {
+    express: server,
+    noCache: true
+});
+
+
+
+// Configuração de caminhos
+// Página Inicial
+// req: Requisição
+// res: Resposta
+server.get("/", (req, res) => {
+    return res.render("index.html", {
+        title: "Um Titulo"
+    });
+});
+
+server.get("/create-point", (req, res) => {
+    return res.render("create-point.html");
+});
+
+server.get("/search", (req, res) => {
+    return res.render("search-results.html");
+});
+
 
 // Startar o Servidor
-server.listen(3000)
+server.listen(3000);
